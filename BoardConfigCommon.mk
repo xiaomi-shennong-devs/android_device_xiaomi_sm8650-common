d8885cc7cbe8a980e0f8b6c45f0218907c90644c
FlowerSea0208 <zzp060208@gmail.com>
2024-04-14 16:19:37 +0800

    sm8650-common: Add Architecture
    Change-Id: I0f7ec248cfd95c117b3924403bb6a9cfae7857f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,6 +9,13 @@ include vendor/xiaomi/sm8650-common/BoardConfigVendor.mk
 
 COMMON_PATH := device/xiaomi/sm8650-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a-branchprot
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo300
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := pineapple
 TARGET_NO_BOOTLOADER := true
